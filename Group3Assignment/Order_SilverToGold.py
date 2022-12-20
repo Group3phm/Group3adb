04899b6b9924403ab0e4b935a7d5653bb1b2e7f0
Group3phm <Group3phm>
2022-12-20 05:22:25 +0000

    Dynamic folder path changes

--- Group3Assignment/Order_SilverToGold.py
+++ Group3Assignment/Order_SilverToGold.py
@@ -1,10 +1,15 @@
 # Databricks notebook source
-df = spark.read.format("csv").load("/mnt/tmpadls/silver/orders_20221216.csv", header = True)
+dbutils.widgets.text("loaddate", "")
+loaddate = dbutils.widgets.get("loaddate")
+
+# COMMAND ----------
+
+df = spark.read.format("csv").load("/mnt/tmpadls/silver/orders_{0}.csv".format(loaddate), header = True)
 display(df)
 
 # COMMAND ----------
 
-df.write.format("parquet").save("/mnt/tmpadls/gold/orders_20221216.csv")
+df.write.format("parquet").save("/mnt/tmpadls/gold/orders_{0}.csv".format(loaddate))
 
 # COMMAND ----------
 
--- Group3Assignment/Product_SilverToGold.py
+++ Group3Assignment/Product_SilverToGold.py
@@ -1,10 +1,15 @@
 # Databricks notebook source
-df = spark.read.format("csv").load("/mnt/tmpadls/silver/Product_20221216.csv", header = True)
+dbutils.widgets.text("loaddate", "")
+loaddate = dbutils.widgets.get("loaddate")
+
+# COMMAND ----------
+
+df = spark.read.format("csv").load("/mnt/tmpadls/silver/Product_{0}.csv".format(loaddate), header = True)
 display(df)
 
 # COMMAND ----------
 
-df.write.format("parquet").save("/mnt/tmpadls/gold/Product_20221216")
+df.write.format("parquet").save("/mnt/tmpadls/gold/Product_{0}".format(loaddate))
 
 # COMMAND ----------
 
